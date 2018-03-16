8949f3678a6a4a19a575ee8228db5c605300d0b7
mjhehn <mjhehn@gmail.com>
2018-03-15 22:56:23 -0600

    added main bot token

--- bot.go
+++ bot.go
@@ -16,7 +16,9 @@ var ars = []*AutoResponse{}
 var mutedServers = []string{}
 
 func main() {
-	discord, err := discordgo.New("Bot " + "NDE1NzMxMDQ0MDgwNDE4ODE2.DW6Lpw.LYY0ZKfCKvC3YMJbCe1u0XpIF7M")
+	//discord, err := discordgo.New("Bot " + "NDE1NzMxMDQ0MDgwNDE4ODE2.DW6Lpw.LYY0ZKfCKvC3YMJbCe1u0XpIF7M")	//dev bot
+	discord, err := discordgo.New("Bot " + "NDA4ODQ1OTY4MDEyNzM4NTYw.DVV_9w.wRs92tvW30aAmW8JOMgqB2GzFQY") //main bot
+
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
