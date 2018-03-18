6b44aeab2e9a1f513ac7f564a429d098f91b1249
mjhehn <mjhehn@gmail.com>
2018-03-18 15:41:45 -0600

    fixed go files from pull

--- commands.go
+++ commands.go
@@ -13,83 +13,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-<<<<<<< HEAD:commands.go
 //rolls a variable number and type of dice. same format of parameters as other responders.
-=======
-var ars = []*AutoResponse{}
-var mutedServers = []string{}
-
-func main() {
-	//discord, err := discordgo.New("Bot " + "NDE1NzMxMDQ0MDgwNDE4ODE2.DW6Lpw.LYY0ZKfCKvC3YMJbCe1u0XpIF7M")	//dev bot
-	discord, err := discordgo.New("Bot " + "NDA4ODQ1OTY4MDEyNzM4NTYw.DVV_9w.wRs92tvW30aAmW8JOMgqB2GzFQY") //main bot
-
-	if err != nil {
-		fmt.Println("error creating Discord session,", err)
-		return
-	}
-	// Register ready as a callback for the ready events.
-	discord.AddHandler(ready)
-
-	discord.AddHandler(messageCreate)
-
-	err = discord.Open()
-	if err != nil {
-		fmt.Println("error opening connection,", err)
-		return
-	}
-
-	readResponseList()
-	//buildResponseList()
-	//writeResponseList()
-
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
-
-	// Cleanly close down the Discord session.
-	discord.Close()
-}
-
-func ready(session *discordgo.Session, message *discordgo.MessageCreate) {
-	session.UpdateStatus(0, "Now with 100% less Python!")
-}
-
-//called every time a message received in a channel the bot is in
-func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate) {
-	// Ignore all messages created by the bot itself(un-needed given the second check) or another bot
-	currentServer := getServer(session, message)
-
-	if message.Author.ID == session.State.User.ID || message.Author.Bot {
-		return
-	}
-
-	responded := make(chan bool)
-
-	go mute(session, message, responded)
-	go unmute(session, message, responded)
-	go mutestatus(session, message, responded)
-	if in(currentServer, mutedServers) {
-		return
-	}
-
-	for _, autoresponse := range ars {
-		if autoresponse.ServerSpecific == nil || in(currentServer, autoresponse.ServerSpecific) {
-			go autoresponse.checkTextResponses(session, message, responded)
-			go autoresponse.checkEmbedResponses(session, message, responded)
-			go autoresponse.checkReactionResponses(session, message, responded)
-		}
-	}
-	go notJustTheMen(session, message, responded)
-	go diceRoller(session, message, responded)
-	go compliment(session, message, responded)
-	go deletemessages(session, message, responded)
-	go cleanup(session, message, responded)
-	<-responded //to synchronize back up with the coroutines
-}
-
->>>>>>> bc7bc2fba6db4a9d52c5c1690dacdf1fdc70b00b:bot.go
 func diceRoller(session *discordgo.Session, message *discordgo.MessageCreate, responded chan bool) {
 	expr, err := regexp.Compile("^!(ir|r)oll [0-9]{0,3}d[0-9]{1,3}$")
 	botutils.Check(err)
@@ -186,12 +110,8 @@ func cleanup(session *discordgo.Session, message *discordgo.MessageCreate, respo
 
 }
 
-<<<<<<< HEAD:commands.go
 //delete a number of messages. same basically as cleanup
 func delete(session *discordgo.Session, message *discordgo.MessageCreate, responded chan bool) {
-=======
-func deletemessages(session *discordgo.Session, message *discordgo.MessageCreate, responded chan bool) {
->>>>>>> bc7bc2fba6db4a9d52c5c1690dacdf1fdc70b00b:bot.go
 	expr, err := regexp.Compile("^!delete [0-9]+$")
 	botutils.Check(err)
 	if expr.MatchString(message.Content) {
