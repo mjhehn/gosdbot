68ee0fd8c7f2fbc3268304f6252950023030fc67
mjhehn <mjhehn@gmail.com>
2018-03-22 23:33:02 -0600

    tweak to cleverbot to pull the api key i was publically pushing up...

--- cleverbot.go
+++ cleverbot.go
@@ -5,16 +5,8 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var clvrbot *cleverbot.Session
-
-func init() {
-	var err error
-	clvrbot, err = cleverbot.New("IGHvKK0w5ozUyNlp", "Y2PWfAYhH43Aa9Fy1gMjg9OxuYFk3w7B")
-	Check(err)
-}
-
 //CleverResponse parses requests from channels tagged with the cleverbot webhook and mentioning this bot, and then asks them to cleverbot.
-func CleverResponse(session *discordgo.Session, message *discordgo.MessageCreate, responded chan bool) {
+func CleverResponse(session *discordgo.Session, message *discordgo.MessageCreate, responded chan bool, clvrbot *cleverbot.Session) {
 	if CheckWebHooks(session, message, "cleverbot") {
 		if (len(message.Mentions) == 1 && message.Mentions[0].ID == session.State.User.ID) || CheckWebHooks(session, message, "nomention") {
 			//send call to the cleverbot.
